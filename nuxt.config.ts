4fd7ca5414fad32134bed6c49a2459caa46f428b
J-Me-2307 <jamie@blaettlers.ch>
2024-09-04 19:40:14 +0200

    Add firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  modules: ['@nuxtjs/tailwindcss']
-})
+  ssr: false,
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-vuefire"],
+
+  vuefire: {
+    auth: {
+      enabled: true
+    },
+    config: {
+      apiKey: "AIzaSyBymfNDuy2Gx3DFtFRhSKHblJxUdRg3Rp0",
+      authDomain: "tasky-20d91.firebaseapp.com",
+      projectId: "tasky-20d91",
+      storageBucket: "tasky-20d91.appspot.com",
+      messagingSenderId: "225173706766",
+      appId: "1:225173706766:web:a67048983caa758221045f",
+    },
+  },
+});
