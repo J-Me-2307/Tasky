4cabb3bd80d2f5380c9e1c8dcbc4aaf65c424315
J-Me-2307 <jamie@blaettlers.ch>
2024-09-15 01:18:37 +0200

    Add current page hint in navbar, add upcoming tasks page

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,28 +18,28 @@
           ref="quickSettings">
           <p class="text-lg block border-b border-text mb-2 pb-2 cursor-default">Quick settings</p>
 
-          <button class="flex items-center p-2 rounded-xl hover:bg-surface1 w-full" @click="navigate('profile')">
+          <button :class="{'bg-surface1': isActive('/profile')}" class="flex items-center p-2 rounded-xl hover:bg-surface1 w-full" @click="navigate('profile')">
             <Icon name="mdi:user-circle" size="25" />
             <span class="ms-3">Profile</span>
           </button>
 
-          <button @click="handleSignOut" class="flex items-center p-2 rounded-xl hover:bg-surface1 w-full">
+          <button :class="{'bg-surface1': isActive('/signout')}" @click="handleSignOut" class="flex items-center p-2 rounded-xl hover:bg-surface1 w-full">
             <Icon name="mdi:logout" size="25" />
             <span class="ms-3">Sign out</span>
           </button>
         </div>
 
-        <button @click="addTask" class="flex items-center p-2 rounded-xl hover:bg-surface1 w-full">
+        <button @click="addTask" class="flex items-center p-2 rounded-xl hover:bg-surface1 w-full my-2">
           <Icon name="mdi:add-circle" size="30" />
           <span class="pl-3">Add task</span>
         </button>
 
-        <button class="flex items-center p-2 rounded-xl hover:bg-surface1 w-full" @click="navigate('/')">
+        <button :class="{'bg-surface1': isActive('/')}" @click="navigate('/')" class="flex items-center p-2 rounded-xl hover:bg-surface1 w-full my-2">
           <Icon name="mdi:calendar-blank" size="30" />
           <span class="pl-3">Today</span>
         </button>
 
-        <button class="flex items-center p-2 rounded-xl hover:bg-surface1 w-full">
+        <button :class="{'bg-surface1': isActive('/upcoming')}" @click="navigate('/upcoming')" class="flex items-center p-2 rounded-xl hover:bg-surface1 w-full my-2">
           <Icon name="mdi:calendar-month" size="30" />
           <span class="pl-3">Upcoming</span>
         </button>
@@ -51,18 +51,18 @@
   </div>
 </template>
 
+
 <script setup>
 import { getAuth, signOut } from 'firebase/auth'
+import { useRoute, useRouter } from 'vue-router'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 
 const user = useCurrentUser();
 const addTaskDialog = ref(false);
-
-useHead({
-  title: 'Tasky'
-})
-
-let quickSettingsIsOpen = ref(false);
+const quickSettingsIsOpen = ref(false);
 const quickSettings = ref(null);
+const route = useRoute(); // Get the current route
+const router = useRouter();
 
 const closeQuickSettingsIfClickedOutside = (event) => {
   if (quickSettingsIsOpen.value && quickSettings.value && !quickSettings.value.contains(event.target)) {
@@ -79,7 +79,6 @@ onBeforeUnmount(() => {
 });
 
 const auth = getAuth();
-const router = useRouter();
 
 const handleSignOut = () => {
   signOut(auth);
@@ -92,5 +91,10 @@ const navigate = (to) => {
 
 const addTask = () => {
   addTaskDialog.value = true;
+}
+
+// Method to check if the current route is active
+const isActive = (path) => {
+  return route.path === path;
 }
 </script>
---
+++ pages/upcoming.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="py-20 px-72 w-full">
+    <div class="mb-10">
+      <div class="mb-16">
+        <p class="text-3xl font-bold">Upcoming</p>
+        <div class="flex items-center space-x-2">
+          <Icon class="leading-none" name="mdi:checkbox-marked-circle-outline" size="15" />
+          <!-- <p class="text-sm leading-1">{{ tasks.length }} Task<span v-if="tasks.length !== 1">s</span></p> -->
+        </div>
+      </div>
+      <ul>
+
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+useHead({
+  title: 'Upcoming - Tasky'
+});
+</script>
